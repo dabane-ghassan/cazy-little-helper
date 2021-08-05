5972f741db79813a4b83b66dc1586bc44434b976
dabane-ghassan <dabane.ghassan@gmail.com>
2021-08-05 11:59:55 +0200

    finishing scorer class

--- cazy_little_helper/predictor.py
+++ cazy_little_helper/predictor.py
@@ -5,6 +5,22 @@ Created on Wed Aug  4 10:38:26 2021
 
 @author: ghassan
 """
+from joblib import load
+from typing import Optional, List
 
-class Predictor:
-    pass
+class Scorer:
+
+    def __init__(
+        self: object,
+        docs: List[str], 
+        model: Optional[str]="../model/cazy_helper.joblib",
+    ) -> None:
+
+        self.model = load(model)
+        self.docs = docs
+        
+    def confidence(
+        self: object
+    ) -> List[float]:
+
+        return self.model.predict_proba(self.docs)[:, 1] * 100
