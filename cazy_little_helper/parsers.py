e963e28d2a51d314723e9d7e223a68454ec73f01
dabane-ghassan <dabane.ghassan@gmail.com>
2021-08-09 11:35:12 +0200

    adding training data

--- cazy_little_helper/parsers.py
+++ cazy_little_helper/parsers.py
@@ -14,7 +14,7 @@ class Parser:
     @staticmethod
     def predict(
     ) -> ArgumentParser:
-        
+
         describe= "Welcome to CAZy's little helper ▼(´ᴥ`)▼ !\n\
     The biocuration assistant of the CAZy database, woof woof.\n\
     CAZy's little helper takes a .csv file that contains a list of PMIDS and \
@@ -39,14 +39,14 @@ class Parser:
                             default=0,
                             help="[OPTIONAL] The index of the ID column in the \
     input file path, default is 0 (first column).")
-    
+
         parser.add_argument('-b','--biblio_add',
                             type=str,
                             required=False,
                             default="http://localhost/Biblio",
                             help="[OPTIONAL] The address of the biblio package \
     on the php server, default is http://localhost/Biblio")
-    
+
         return parser
 
     @staticmethod
@@ -62,23 +62,23 @@ class Parser:
     type of ids.\n\
     Please visit https://github.com/dabane-ghassan/cazy-little-helper \
     for more information about the package."
-    
+
         parser = ArgumentParser(add_help=True,
                                          description=describe,
                                          formatter_class=RawTextHelpFormatter)
-    
+
         parser.add_argument('-i','--input_path',
                             type=str,
                             required=True,
                             default=sys.stdin,
                             help="[REQUIRED] The input ID file path, \
     a .csv file with a column of article IDs")
-    
+
         parser.add_argument('-t','--id_type',
                             type=str,
                             required=False,
                             default=sys.stdin,
                             help="[REQUIRED] The type of ID to find, \
     ['PMID', 'PMCID', 'DOI'], uppercase only")
-    
+
         return parser
