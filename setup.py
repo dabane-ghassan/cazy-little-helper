651a0b1e0da573ed9938b860c260f71bfa0d73ed
dabane-ghassan <dabane.ghassan@gmail.com>
2021-08-11 16:23:45 +0200

    fixing requirements setuip

--- setup.py
+++ setup.py
@@ -2,25 +2,28 @@ import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setuptools.setup(
     name='cazy-little-helper',
-    version='1.0',
+    version='1.1.1',
     description='A biocuration assistant for the CAZy database.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/dabane-ghassan/cazy-little-helper',
     author='dabane-ghassan',
     author_email = 'dabane.ghassan@gmail.com',
-    download_url = 'https://github.com/dabane-ghassan/cazy-little-helper/archive/refs/tags/v1.0.tar.gz',
+    download_url = 'https://github.com/dabane-ghassan/cazy-little-helper/archive/refs/tags/v1.1.1tar.gz',
     license='MIT',
-    packages=setuptools.find_packages(include=['cazy-little-helper', 'cazy-little-helper.*']),
+    packages=setuptools.find_packages(include=['cazy_little_helper', 'cazy_little_helper.*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=requirements.txt,
+    install_requires=required,
     extras_require={
         'dev': [
             'pytest >= 6.0.0',
